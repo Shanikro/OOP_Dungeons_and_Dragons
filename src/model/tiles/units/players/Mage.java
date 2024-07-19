cca85fcaeaec569acd59a7425b29a37c238948f0
Gaya Aviv <gayaav@post.bgu.ac.il>
2024-07-19 19:15:14 +0300

    finish players

--- src/model/tiles/units/players/Mage.java
+++ src/model/tiles/units/players/Mage.java
@@ -1,5 +1,11 @@
 package model.tiles.units.players;
 
+import model.tiles.units.enemies.Enemy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Mage  extends Player{
 
     private final int MAGE_ADDITIONAL_MANA_CAP = 25;
@@ -12,16 +18,18 @@ public class Mage  extends Player{
     private int spellPower;
     private int hitsCount;
     private int abilityRange;
+    private Random random;
 
 
-    public Mage(String name, int hitPoints, int attack, int defense, int manaCap, int manaCost, int spellPower, int hitsCount, int abilityRange) {
-        super(name, hitPoints, attack, defense);
+    public Mage(String name, int health, int attack, int defense, int manaCap, int manaCost, int spellPower, int hitsCount, int abilityRange) {
+        super(name, health, attack, defense);
         this.manaCap = manaCap;
         this.manaCurr = manaCap/4;
         this.manaCost = manaCost;
         this.spellPower = spellPower;
         this. hitsCount = hitsCount;
         this.abilityRange = abilityRange;
+        this.random = new Random();
     }
 
     public void levelUp(){
@@ -37,12 +45,43 @@ public class Mage  extends Player{
     }
 
     //use special ability
-    public void useSA(){
+    public void useSA(List<Enemy> enemies){
         this.manaCurr -= this.manaCost ;
         int hits = 0;
-       /* while (hits< hitsCount && ) {
+        while (hits < hitsCount && hasLivingEnemyInRange(enemies)) {
+            Enemy enemy = selectRandomEnemyInRange(enemies);
+            if (enemy != null) {
+                int damage = this.spellPower;
+                int actualDamage = Math.max(0, damage - enemy.defend());
+                enemy.getHealth().setCurrent(enemy.getHealth().getCurrent() - actualDamage);
+               // System.out.println("Enemy " + enemy.getName() + " took " + actualDamage + " damage.");
+                hits++;
+            }
+        }
 
-        }*/
+    }
+    private boolean hasLivingEnemyInRange(List<Enemy> enemies) {
+        for (Enemy enemy : enemies) {
+            if (enemy.getHealth().getCurrent() > 0 && this.position.range(enemy.getPosition()) < this.abilityRange) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    private Enemy selectRandomEnemyInRange(List<Enemy> enemies) {
+        List<Enemy> enemiesInRange = new ArrayList<>();
+        for (Enemy enemy : enemies) {
+            if (enemy.getHealth().getCurrent() > 0 && this.position.range(enemy.getPosition()) < this.abilityRange) {
+                enemiesInRange.add(enemy);
+            }
+        }
+
+        if (enemiesInRange.isEmpty()) {
+            return null;
+        }
+
+        int index = random.nextInt(enemiesInRange.size());
+        return enemiesInRange.get(index);
     }
 }
