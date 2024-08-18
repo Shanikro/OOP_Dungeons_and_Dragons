85201b837ad1395d9ff633e3bd43bdbf1ad9612c
Gaya Aviv <gayaav@post.bgu.ac.il>
2024-08-18 11:02:11 +0300

    m

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import model.game.Game;
 import model.tiles.units.Unit;
 import model.tiles.units.players.Player;
 import utils.Position;
+import utils.callbacks.MessageCallback;
 import utils.generators.FixedGenerator;
+import view.CLI;
+import view.View;
 
 public class Main {
     public static void main(String[] args) {
-        Unit p = new Player("Player", 100, 10, 10)
-                .initialize(new Position(0, 0), new FixedGenerator(), () -> System.out.println("Player died"), System.out::println);
+        View cliView = new CLI();
+        MessageCallback callback = cliView.getCallback();
+
+        String levelDirPath;
+
+        if(args.length > 0){
+            levelDirPath = args[0];
+            Game game = new Game(levelDirPath, callback);
+            game.start();
+        }
         System.out.println("Hello world!");
     }
 }
--- src/model/game/Game.java
+++ src/model/game/Game.java
@@ -1,4 +1,71 @@
 package model.game;
 
+import control.initializers.TileFactory;
+import utils.callbacks.MessageCallback;
+import utils.printer.PrinterC;
+
+import java.util.Scanner;
+
 public class Game {
+    private Level currentLevel;
+    private String directoryPath;
+    private final TileFactory factory = TileFactory.getInstance();
+    private MessageCallback msg;
+    private PrinterC printer;
+
+
+    public Game(String levels, MessageCallback callback){
+            this.msg = callback;
+            
+            this.currentLevel = new Level(msg);
+            this.directoryPath = levels;
+            this.printer = new PrinterC();
+            ()-> printer.print("notice you must put the level_dir folder");
+            msg.send("you put it? enter 0 for continue");
+            Scanner scanner = new Scanner(System.in);
+            int validfolder = scanner.nextInt();
+            if (validfolder == 0) {
+                factory.printPlayers();
+                msg.send("Choose your player from the list");
+                int playerChosen = scanner.nextInt();
+                while(playerChosen < 1 || playerChosen > 6) {
+                    msg.send("Invalid player chosen");
+                    playerChosen = scanner.nextInt();
+                }
+                currentLevel.choosePlayer(tileFactory.getPlayer(playerChosen));
+            }
+            else{
+                msg.send("reload the game");
+            }
+
+
+        }
+
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
+        int levelNumber = 1;
+
+        while (!currentLevel.gameOver() && currentLevel.hasLevel(directoryPath + "\\level" + levelNumber + ".txt")) {
+            currentLevel.loadLevel(directoryPath + "\\level" + levelNumber + ".txt");
+
+            while (!currentLevel.gameOver() && !currentLevel.isOver()) {
+                EmptyRow();
+                currentLevel.levelInfo();
+                msg.send("Your turn - ");
+                String userAction = scanner.nextLine();
+                currentLevel.gameTick(userAction);
+            }
+
+            levelNumber++;
+        }
+
+        msg.send("\n GAME OVER!!!");
+    }
+
+    public void EmptyRow() {
+        msg.send("");
+    }
+}
+
+    }
 }
